5b944a9bee8cbb5c557bd771cc85d2aab1a458a2
U170709041 <ayhanyuksek@posta.mu.edu.tr>
2021-01-16 19:45:58 +0300

    data encoding yaptık ve görselleştirme işlemlerine başlanıldı.

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import warnings
+from sklearn import preprocessing
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 
 warnings.filterwarnings('ignore')
 
@@ -36,6 +41,10 @@ for i in range(dataset.shape[0]):
     if dataset["Age"][i] < 18 or dataset["Age"][i] > 120:
         dataset["Age"][i] = median
 
+# Creating age_range feature.
+dataset['age_range'] = pd.cut(dataset['Age'], [0, 20, 30, 65, 100], labels=["0-20", "21-30", "31-65", "66-100"],
+                              include_lowest=True)
+
 # Create 3 arrays for genders (male, female, trans)
 male = ["male", "m", "male-ish", "maile", "mal", "male (cis)", "make", "male ", "man", "msle", "mail", "malr",
         "cis man", "cis male", "p"]
@@ -59,4 +68,50 @@ for i in range(dataset.shape[0]):
     elif dataset["Gender"][i] in trans:
         dataset["Gender"][i] = "trans"
 
-print(dataset["Gender"].unique())
+# 3. Encoding dataset
+
+# Backup dataset for graph label
+backupDataset = dataset.copy()
+
+# Encoding dataset with LabelEncoder()
+for i in dataset:
+    label_encoder = preprocessing.LabelEncoder()
+    label_encoder.fit(dataset[i])
+    dataset[i] = label_encoder.transform(dataset[i])
+
+# Checking whether there are any missing values or not.
+print(dataset.isnull().sum())
+
+# 4. Plotting charts for dataset visualization
+
+# Distribution by Age
+mu = backupDataset["Age"].mean()
+sigma = backupDataset["Age"].std()
+
+num_bins = backupDataset["Age"].unique().size
+
+fig, ax = plt.subplots()
+n, bins, patches = ax.hist(backupDataset["Age"], num_bins, density=True)
+
+y = ((1 / (np.sqrt(2 * np.pi) * sigma)) *
+     np.exp(-0.5 * (1 / sigma * (bins - mu)) ** 2))
+ax.plot(bins, y, '--')
+ax.set_xlabel('Age')
+ax.set_title("Distribution by Age")
+
+fig.tight_layout()
+# plt.show()
+
+# Mental Health Condition By Family History And Gender
+sns.catplot(x="family_history", y="treatment", hue="Gender", data=dataset, kind="bar").set_xticklabels(["YES", "NO"])
+plt.title('Mental Health Condition By Family History And Gender')
+plt.xlabel('Family History')
+
+# Mental Health Condition By Age Range
+sns.catplot(x="age_range", y="treatment", hue="Gender", data=dataset, kind="bar", ci=None).set_xticklabels(
+    ["0-20", "21-30", "31-65", "66-100"])
+plt.title('Mental Health Condition By Age Range')
+plt.xlabel('Family History')
+
+plt.show()
+
